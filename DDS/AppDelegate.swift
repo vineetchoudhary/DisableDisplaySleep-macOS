e0c52b69f32f0d77bfaca6d4a01df0ed025c3694
Vineet Choudhary <vineetchoudhary@live.in>
2024-03-20 13:07:46 +0530

    Remove default menus

--- DDS/AppDelegate.swift
+++ DDS/AppDelegate.swift
@@ -9,6 +9,11 @@ import Foundation
 import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		let requiredAppMenus = ["DDS"]
+		NSApp.mainMenu?.items.removeAll { !requiredAppMenus.contains($0.title) }
+	}
+
 	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
 		true
 	}
