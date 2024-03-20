bf78cf20bf4d951161d3038ef551be78baf3305b
Vineet Choudhary <vineetchoudhary@live.in>
2024-03-20 12:30:03 +0530

    Set default window size and min width height for main view

--- DDS/OneDSApp.swift
+++ DDS/OneDSApp.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 @main
 struct OneDSApp: App {
+	private struct LayoutConstants {
+		static let defaultWidth: CGFloat = 650.0
+		static let defaultHeight: CGFloat = 350.0
+	}
+
 	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-			MainView()
-				.navigationTitle("Disable Display Sleep")
-				.frame(minWidth: 650, idealWidth: 650, maxWidth: 650, minHeight: 350, idealHeight: 350, maxHeight: 350)
-        }
+		rootView()
     }
+
+	private func rootView() -> some Scene {
+		WindowGroup {
+			MainView()
+				.frame(
+					minWidth: LayoutConstants.defaultWidth,
+					idealWidth: LayoutConstants.defaultWidth,
+					minHeight: LayoutConstants.defaultHeight,
+					idealHeight: LayoutConstants.defaultHeight)
+		}
+		.defaultSize(width: LayoutConstants.defaultWidth, height: LayoutConstants.defaultHeight)
+	}
 }
